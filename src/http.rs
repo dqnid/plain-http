a0a11637f2c049788bf2c8b5f1b4d81d5bad4d55
dqnid <dani.heras@hotmail.com>
2024-11-23 00:29:20 +0100

    feat(request_parser): base code

--- src/http.rs
+++ src/http.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::io::prelude::*;
 use std::net::TcpStream;
@@ -5,6 +6,19 @@ use std::net::TcpStream;
 pub struct ProcessedResponse {
     pub data: String,
     status: u8,
+}
+
+struct HttpRequestLine {
+    method: String,
+    version: f32,
+    path: String,
+    params: HashMap<String, String>,
+}
+
+struct HttpRequest {
+    request: HttpRequestLine,
+    headers: HashMap<String, String>,
+    body: String,
 }
 
 pub fn process_petition(stream: &mut TcpStream) -> std::io::Result<ProcessedResponse> {
@@ -35,4 +49,24 @@ pub fn process_petition(stream: &mut TcpStream) -> std::io::Result<ProcessedResp
     };
 
     Ok(response)
+}
+
+fn parse_request(request_raw: String) -> Result<HttpRequest, i16> {
+    // TODO: study if better to use match
+    if let Some((heading, rest)) = request_raw.split_once("\n") {
+        // Process heading
+        // split heading with split_whitespace
+        // for (i, line) in request_raw.enumerate() {
+        // }
+        if let Some((headers, body)) = rest.split_once("\n\n") {
+            // Process headers and body
+            // split headers over ":"
+        }
+    }
+
+    Err(400)
+}
+
+fn parse_request_line(request_line: String) -> HttpRequestLine {
+    if let Some((method, path, version)) = request_line.split_whitespace() {}
 }
